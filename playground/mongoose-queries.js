841d3286fa51ee83ff7197ba9678359d187b1fa2
maheshpote <maheshpote.inmac@gmail.com>
2018-10-01 17:46:36 +0530

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+const { ObjectID } = require('mongodb');
+
+const { mongoose } = require('./../server/db/mongoose');
+const { Todo } = require('./.././server/models/todo');
+const { User } = require('./.././server/models/user');
+
+//var id = '5bb200a65ab7a7076cf5fac4980011';
+var id = '5baf0cfd1fae3f1de08a2369005';
+if (!ObjectID.isValid(id)) {
+    console.log('Id not valid.');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Find Todos:', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Find One:', todo);
+// });
+
+// Todo.findById({
+//     _id: id
+// }).then((tobyid) => {
+//     if (!tobyid) {
+//         return console.log("Id not found");
+//     }
+//     console.log('Find by id:', tobyid);
+// }).catch((err) => console.log(err));
+
+User.findById({
+    _id: id
+}).then((users) => {
+    if (!users) {
+        return console.log("User not found");
+    }
+    console.log('User Found: ', users);
+});
