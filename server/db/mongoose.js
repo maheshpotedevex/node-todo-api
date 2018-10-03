a996cd6e48bcefe3247d7e557e21f92c7f40d74f
maheshpote <maheshpote.inmac@gmail.com>
2018-10-03 15:10:22 +0530

    heroku db update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // Built in promise library.
 //mongoose.connect('mongodb://localhost:27017/TodoAppMongoose', { useNewUrlParser: true });
-mongoose.connect('https://mlab.com/databases/todoapp', { useNewUrlParser: true });
+mongoose.connect('mongodb://<maheshpote>:<mlabtodo@1>@ds121593.mlab.com:21593/todoapp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
