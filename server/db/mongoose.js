a986f3d25df09fb9cba05f835a4adcf859d46436
maheshpote <maheshpote.inmac@gmail.com>
2018-10-03 17:30:38 +0530

    Add localhost url in /mongoose2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise; // Built in promise library.
 // Remote MongoDB url by mlab 
 const mlaburl = `MONGOLAB_URI: mongodb://maheshpote:mlab789@ds121593.mlab.com:21593/todoapp`;
 const localhost = `mongodb://maheshpote:mlabtodo@1@ds121593.mlab.com:21593/todoapp`;
-mongoose.connect(`mlaburl || localhost`, { useNewUrlParser: true });
+mongoose.connect(mlaburl, { useNewUrlParser: true });
 //mongoose.connect('mongodb://maheshpote:mlabtodo@1@ds121593.mlab.com:21593/todoapp', { useNewUrlParser: true });
 
 module.exports = { mongoose };
