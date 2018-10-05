063f5182d511f874ff86639161c4931871e86549
maheshpote <maheshpote.inmac@gmail.com>
2018-10-05 12:46:34 +0530

    Modified config port

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // Built in promise library.
 // Remote MongoDB url by mlab 
-const mlaburl = `MONGOLAB_URI:mongodb://maheshpote:mlab789@ds121593.mlab.com:21593/todoapp`;
+//const mlaburl = `MONGOLAB_URI:mongodb://maheshpote:mlab789@ds121593.mlab.com:21593/todoapp`;
 const localhost = `mongodb://127.0.0.1:27017/TodoAppMongoose`;
-mongoose.connect(mlaburl || localhost, { useNewUrlParser: true });
+mongoose.connect(localhost, { useNewUrlParser: true });
 //mongoose.connect('mongodb://localhost:27017/TodoAppMongoose', { useNewUrlParser: true });
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
-//const port = 3000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Tirdparty middleware
 app.use(bodyParser.json());
